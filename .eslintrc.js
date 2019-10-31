4e4b59302c6f044e596f8d6c95d4e974115eb3c1
asimashfaq <asimizb4@gmail.com>
2019-10-31 14:00:22 +0500

    Add the config

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+
+    env: {
+        node: true,
+        commonjs: true,
+        es6: true
+    },
+
+    extends: [
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended' // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+
+    rules: {
+        semi: ['error', 'never'],
+        'no-unused-expressions': 'off',
+        indent: [2, 2, { SwitchCase: 1 }],
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/explicit-function-return-type': [
+            'error',
+            {
+                allowExpressions: true
+            }
+        ]
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        ecmaVersion: 2018
+    }
+}
